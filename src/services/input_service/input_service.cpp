b16a25552080960d06cb86d267a96bdedfd95ff2
Robert Vollmer <dev@robv.de>
2025-04-13 23:19:32 +0000

    Initialize and call drivers from InputService

--- src/services/input_service/input_service.cpp
+++ src/services/input_service/input_service.cpp
@@ -1,8 +1,47 @@
 #include "input_service.hpp"
 
+#include <etl/string.h>
+
+#include <xbot-service/Lock.hpp>
+
+#include "globals.hpp"
+#include "ulog.h"
+#include "zcbor_helpers.h"
+
+using xbot::service::Lock;
+
 bool InputService::OnStart() {
+  // Start drivers.
+  for (auto& driver : drivers_) {
+    if (!driver.second->OnStart()) {
+      ULOG_ERROR("Failed to start input driver %d", driver.first);
+      return false;
+    }
+  }
+
+  // FIXME: This should be read from the config.
+  const InputConfig* input_configs = nullptr;
+
+  // Parse input configurations and pass them to the drivers.
+  Lock lk(&mutex);
+  all_inputs_.clear();
+  for (size_t i = 0; i < 1; i++) {
+    auto& input_config = input_configs[i];
+    auto it = drivers_.find(input_config.driver_choice);
+    if (it == drivers_.end()) {
+      ULOG_ERROR("Unknown input driver %d for %s", input_config.driver_choice, to_string_view(input_config.name));
+      continue;
+    }
+    Input* input = it->second->AddInput(input_config);
+    all_inputs_.push_back(input);
+  }
+
   return true;
 }
 
 void InputService::tick() {
+  // TODO: Check if these can run at the same frequency.
+  for (auto& driver : drivers_) {
+    driver.second->tick();
+  }
 }
--- src/services/input_service/input_service.hpp
+++ src/services/input_service/input_service.hpp
@@ -1,8 +1,13 @@
 #ifndef INPUT_SERVICE_HPP
 #define INPUT_SERVICE_HPP
 
-#include <InputServiceBase.hpp>
+#include <etl/flat_map.h>
 
+#include <InputServiceBase.hpp>
+#include <drivers/input/gpio_input_driver.hpp>
+#include <drivers/input/worx_input_driver.hpp>
+
+using namespace xbot::driver::input;
 using namespace xbot::service;
 
 class InputService : public InputServiceBase {
@@ -10,7 +15,23 @@ class InputService : public InputServiceBase {
   explicit InputService(uint16_t service_id) : InputServiceBase(service_id, wa, sizeof(wa)) {
   }
 
+  mutex_t mutex;
+
+  const etl::ivector<Input*>& GetAllInputs() const {
+    return all_inputs_;
+  }
+
  private:
+  GpioInputDriver gpio_driver_;
+  WorxInputDriver worx_driver_;
+
+  const etl::flat_map<uint8_t, InputDriver*, 2> drivers_ = {
+      {InputConfig::InputConfig_driver_gpio_tstr_c, &gpio_driver_},
+      {InputConfig::InputConfig_driver_worx_tstr_c, &worx_driver_},
+  };
+
+  etl::vector<Input*, 30> all_inputs_;
+
   bool OnStart() override;
   void tick();
   ManagedSchedule tick_schedule_{scheduler_, IsRunning(), 20'000,
